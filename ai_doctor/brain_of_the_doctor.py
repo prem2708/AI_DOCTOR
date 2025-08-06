3d6de14b2f8d76840432dedb2c951111ac5e9048
prem2708 <preminnovator2708@gmail.com>
2025-08-06 23:11:21 +0530

    newproj

---
+++ ai_doctor/brain_of_the_doctor.py
@@ -0,0 +1,37 @@
+
+
+#Step1: Setup GROQ API key
+import os
+
+GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
+
+#Step2: Convert image to required format
+import base64
+
+
+#image_path="acne.jpg"
+
+def encode_image(image_path):   
+    image_file=open(image_path, "rb")
+    return base64.b64encode(image_file.read()).decode('utf-8')
+
+#Step3: Setup Multimodal LLM 
+from groq import Groq
+
+query="Is there something wrong with my face?"
+model="llama-3.3-70b-versatile"
+
+def analyze_image_with_query(query, model, encoded_image):
+    client=Groq()  
+    messages=[
+        {
+            "role": "user",
+            "content": query
+        }
+    ]
+    chat_completion=client.chat.completions.create(
+        messages=messages,
+        model=model
+    )
+
+    return chat_completion.choices[0].message.content
